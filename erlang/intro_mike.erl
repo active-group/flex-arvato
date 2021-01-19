2f48eb98b20eb12c3bfca3f9d82db23a15467588
Mike Sperber <sperber@deinprogramm.de>
2021-01-19 12:36:55 +0100

    Update.

--- erlang/intro_mike.erl
+++ erlang/intro_mike.erl
@@ -94,5 +94,5 @@ d2() -> #dillo{liveness = dead, weight =12}.
 % Gürteltier überfahren
 % class Dillo { void runOver() { this.alive = false; } }
 -spec run_over_dillo(#dillo{}) -> #dillo{}.
-run_over_dillo(#dillo{liveness = _, weight = Weight}) ->
+run_over_dillo(#dillo{weight = Weight}) ->
     #dillo{liveness = dead, weight = Weight}.
