546e40375dd84a90fde2aa4b0756068b8ebf2805
Mike Sperber <sperber@deinprogramm.de>
2021-01-19 10:52:38 +0100

    Update.

--- erlang/intro_mike.erl
+++ erlang/intro_mike.erl
@@ -12,5 +12,5 @@
 -spec double(number()) -> number().
 double(X) -> X * 2.
 
--spec double(number(), number()) -> number().
+% -spec double(number(), number()) -> number().
 double(X, Y) -> X * Y * 2.
