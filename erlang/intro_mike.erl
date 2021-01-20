31230ecce42efe65f20c785c3367e8e6d9b22c90
Mike Sperber <sperber@deinprogramm.de>
2021-01-20 10:49:13 +0100

    Update.

--- erlang/intro_mike.erl
+++ erlang/intro_mike.erl
@@ -272,6 +272,7 @@ calc_supervisor() ->
     % wenn ein gelinkter Prozess stirbt, bekomme ich eine Nachricht
     % {'EXIT', Pid, Exception}
     register(calc_service, Pid),
+    global:register()
     receive
         {'EXIT', _FromPid, _Reason} -> calc_supervisor()
         % _Msg -> calc_supervisor() % Mmmhh ...
