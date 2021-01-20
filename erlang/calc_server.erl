9e2f583b0403932e4b9f3d853e1774aed5cd294a
Mike Sperber <sperber@deinprogramm.de>
2021-01-20 11:07:43 +0100

    Update.

--- erlang/calc_server.erl
+++ erlang/calc_server.erl
@@ -4,3 +4,10 @@
 
 % Schnittstelle für Callbacks
 % Hier eine Implementierung des Interface gen_server
+
+-record(reset, {}).
+-record(inc, {increment :: number()}).
+-record(mult, {factor :: number()}).
+-record(divide, {divisor :: number()}).
+-record(get, {pid :: pid()}).
+
