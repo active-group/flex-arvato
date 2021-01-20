3e1d8406a8930f59ed909bcc9e576a9d74b05b0b
Mike Sperber <sperber@deinprogramm.de>
2021-01-20 09:55:18 +0100

    Update.

--- erlang/intro_mike.erl
+++ erlang/intro_mike.erl
@@ -266,6 +266,6 @@ calc_process() ->
     Pid = spawn(?MODULE, calc_loop, [0]),
     % wenn Pid stirbt, sterbe auch ich (und umgekehrt)
     link(Pid),
-    process_flag(trap_exit),
+    process_flag(trap_exit, true),
     register(calc_service, Pid),
     Pid.
